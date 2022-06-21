691ea638b8f77532e63e1401a03a92ad437ebd6d
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-06-21 20:07:30 +0200

    add event listeners to created grid elements
    they allow to modify css properties on mouseover

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const gridContainer = document.querySelector(".grid-container")
+const highlightStyle = `
+    background-color: red;
+`
+let editingActive = false
+
+gridContainer.addEventListener("mousedown", e => {
+    editingActive = true
+})
+gridContainer.addEventListener("mouseup", e => {
+    editingActive = false
+})
 
 function createGridElements(sideLength) {
     for (let i = 0; i < sideLength ** 2; i++) {
         const newElement = document.createElement("div")
+        newElement.addEventListener("mouseover", e => {
+            if (editingActive) {
+                newElement.style.cssText = highlightStyle
+            }
+        })
         gridContainer.appendChild(newElement)
+
     }
 }
 
