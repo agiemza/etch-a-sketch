eee093b60ad15f2573741e2ceab5a36b0e2e2558
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-06-22 19:24:53 +0200

    add random color button

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const rangeInput = document.querySelector(".range-input")
 const currentRangeContainer = document.querySelector(".current-range-container")
 const colorPicker = document.querySelector(".color-picker")
 const resetButton = document.querySelector(".reset-button")
+const randomColorButton = document.querySelector(".random-color")
 
 const settings = {
-    penColor: "rgba(0,0,0,1)",
-    canvasColor: "rgba(255,255,255,1)",
+    penColor: "#000000",
+    canvasColor: "#ffffff",
     editingActive: false,
     currentTargetColor: undefined,
     boxSide: undefined,
@@ -21,10 +22,21 @@ function setEventListeners() {
     })
     gridContainer.addEventListener("mouseup", disableEditing)
     gridContainer.addEventListener("mouseleave", disableEditing)
-    
+
     rangeInput.addEventListener("change", changeGridSize)
     colorPicker.addEventListener("change", e => {
         settings.penColor = e.target.value
+    })
+    randomColorButton.addEventListener("click", () => {
+        let newColor = {
+            red: (`0${Math.floor(Math.random() * 256).toString(16)}`).slice(-2),
+            green: (`0${Math.floor(Math.random() * 256).toString(16)}`).slice(-2),
+            blue: (`0${Math.floor(Math.random() * 256).toString(16)}`).slice(-2)
+        }
+        console.log(`#${newColor.red}${newColor.green}${newColor.blue} `)
+        settings.penColor = `#${newColor.red}${newColor.green}${newColor.blue}`
+        colorPicker.value = `#${newColor.red}${newColor.green}${newColor.blue}`
+
     })
     resetButton.addEventListener("click", resetGrid)
 }
@@ -34,8 +46,8 @@ function createGridElements(sideLength) {
     for (let i = 0; i < sideLength ** 2; i++) {
         const newElement = document.createElement("div")
         newElement.style.cssText = `
-            width: ${settings.boxSide}; 
-            height: ${settings.boxSide};
+        width: ${settings.boxSide};
+        height: ${settings.boxSide};
         `
         newElement.addEventListener("mouseover", handleMouseOver)
         newElement.addEventListener("mouseout", handleMouseLeave)
@@ -56,17 +68,17 @@ function handleMouseOver(e) {
 function handleMouseLeave(e) {
     if (!settings.editingActive) {
         e.target.style.cssText = `
-            width: ${settings.boxSide}; 
-            height: ${settings.boxSide}; 
-            background-color: ${settings.currentTargetColor};
-        `
+        width: ${settings.boxSide};
+        height: ${settings.boxSide};
+        background-color: ${settings.currentTargetColor};
+    `
     }
 }
 
 function paintGridElements(e) {
     e.target.style.cssText = `
-        width: ${settings.boxSide}; 
-        height: ${settings.boxSide}; 
+        width: ${settings.boxSide};
+        height: ${settings.boxSide};
         background-color: ${settings.penColor};
     `
 }
